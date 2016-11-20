05e68374dffbd87c7ae80187a97e89dd7e9d4789
Deepa Umakanth <udeepa@Deepas-MacBook-Air.local>
2016-11-20 15:14:26 -0700

    our work pages

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,8 +1,14 @@
-/**
- * Created by vipul on 4/23/2016.
- */
+
 exports.renderHome = function(req, res) {
     res.render('index', {
+        title: "Ffutche Foundation",
+        user: JSON.stringify(req.user)
+    });
+
+};
+
+exports.ourwork = function(req, res) {
+    res.render('ourwork', {
         title: "Ffutche Foundation",
         user: JSON.stringify(req.user)
     });
