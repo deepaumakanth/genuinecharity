ea723db1a71e5d544c72649e2ef0e81080b7de62
Deepa Umakanth <udeepa@Deepas-MacBook-Air.local>
2016-11-20 23:07:03 -0700

    add scholarship

--- app/controllers/scholarship.server.controller.js
+++ app/controllers/scholarship.server.controller.js
@@ -1,4 +1,5 @@
 
+var sequelize = require("../../config/sequelize").getSequelize;
 
 exports.sponsorscholarship = function(req, res) {
     res.render('sponsorscholarship', {
@@ -17,20 +18,28 @@ exports.applyscholarship = function(req, res) {
 };
 
 exports.addsponsor = function(req, res) {
+	var name =  req.body.name,
+      amount = req.body.amount,
+      prerequisite = req.body.prerequisite,
+      expiry = req.body.expiry,
+      type = req.body.type;
 
-	var scholarship = {
-      name: req.params.name,
-      amount: req.params.amount
+              var query = "insert into scholarships (name, amount,prerequisite,expiry,type) values (:name, :amount, :prerequisite, :expiry,:type)";
+              sequelize.query(query, { replacements: {name: name, amount: amount, prerequisite: prerequisite, expiry: expiry, type: type }})
+            .then(function(success) {
+                console.log("add scholarships"+JSON.stringify(success));
+                return res.status(200).send({success:"success"});
+            }).catch(function(err){
+                console.log("scholarships could not be added"+JSON.stringify(err));
+                return res.status(401).send({ error:err })
+            });
+
+            res.render('applyscholarship', {
+        title: "Ffutche Foundation",
+        userFirstName: req.user ? req.user.firstname : '',
+        userLastName: req.user ? req.user.lastname : ''
+    });
     
   };
-  console.log("dpa:"+scholarship);
-
-    res.render('addsponsorconfirmation', {
-     title: "Ffutche Foundation",
-        user: JSON.stringify(req.user)
-     });
-
-    
-
-};
+  
 	
