c56d9828570fa3ac85ab501abbffd9b58f0ddbed
Deepa Umakanth <udeepa@Deepas-MacBook-Air.local>
2016-11-21 01:07:55 -0700

    insert into users_sponsor code

--- app/controllers/scholarship.server.controller.js
+++ app/controllers/scholarship.server.controller.js
@@ -23,16 +23,28 @@ exports.addsponsor = function(req, res) {
       prerequisite = req.body.prerequisite,
       expiry = req.body.expiry,
       type = req.body.type;
+     // email_id =  req.user.email_id;
+
+                //console.log("Email Id:"+ req.user.email_id);
+
 
               var query = "insert into scholarships (name, amount,prerequisite,expiry,type) values (:name, :amount, :prerequisite, :expiry,:type)";
               sequelize.query(query, { replacements: {name: name, amount: amount, prerequisite: prerequisite, expiry: expiry, type: type }})
             .then(function(success) {
-                console.log("add scholarships"+JSON.stringify(success));
+              console.log("deepa");
+                //return res.status(200).send({success:"success"});
+                var query = "insert into user_sponsors_scholarship (email_id, sponsorship_id) values (:email_id, :sponsorship_id)";
+                sequelize.query(query, { replacements: {email_id: email_id, sponsorship_id: sponsorship_id }})
+                .then(function (success) {
                 return res.status(200).send({success:"success"});
+
+                });
+
             }).catch(function(err){
                 console.log("scholarships could not be added"+JSON.stringify(err));
                 return res.status(401).send({ error:err })
             });
+            
 
         res.render('applyscholarship', {
         title: "Ffutche Foundation",
